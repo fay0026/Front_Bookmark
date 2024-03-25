3b83dafe90745ba5fa5c6119bd62bd4cc113ea59
Damien FAY <Damien.fay@etudiant.univ-reims.fr>
2024-03-25 11:59:13 +0100

    TP3.1 Ajout du routeur

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,18 +1,27 @@
 import React from "react";
-import Index from "./routes/index";
+import { Route, Switch } from "wouter";
+import BookmarksList from "../components/BookmarksList";
 
-function App() {
+function Index() {
+  return (
+    <Switch>
+      <Route path="/" component={BookmarksList} />
+    </Switch>
+  );
+}
+
+/* function index() {
   return (
     <div className="app">
       <header className="app__header header">
         <h1 className="header__title">Introduction to React</h1>
       </header>
       <main className="app__main">
-        <Index />
+        <BookmarksList />
       </main>
       <footer className="app__footer footer">footer</footer>
     </div>
   );
-}
+} */
 
-export default App;
+export default Index;
